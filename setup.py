2088c99dabd4ca11e4a9caf2aac6c0135fd18466
sohel10 <sohelcu06@gmail.com>
2024-10-03 08:18:03 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Sohel Ahmed',
+    author_email='sohelcu06@gmail.com',
+    packages=find_packages(),
+    install_requires=[]  # Add any package-specific dependencies here, if any.
+)
